4bf586c5663c17061ab9f2b9798e067de95562a2
codingmInt <sehoonk01291@gmial.com>
2022-07-02 12:38:52 -0400

    .

--- guis.py
+++ guis.py
@@ -1,7 +1,19 @@
-import tkinter
+from tkinter import *
+from tkinter import ttk
 
-window = tkinter.Tk()
-window.title('CALC GUI')
-window.geometry("200x200")
+def enter_pressed():
+    print(entry_value.get())
+    writing_entry.delete(0, 'end')
 
-window.mainloop()
+root = Tk()
+root.title("mINt gUI")
+root.geometry('480x360')
+
+entry_value = StringVar(root, value='')
+
+writing_entry = ttk.Entry(root, textvariable=entry_value, width=20)
+writing_entry.grid(row=0,columnspan=3)
+enter_btn = ttk.Button(root, text='entr', command=enter_pressed)
+enter_btn.grid(row=0, column=4)
+
+root.mainloop()
