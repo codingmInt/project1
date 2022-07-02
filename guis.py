cafa8775f656a394786a463f5e12fd292a79b92c
codingmInt <sehoonk01291@gmial.com>
2022-07-02 15:04:38 -0400

    .

--- guis.py
+++ guis.py
@@ -1,8 +1,10 @@
 from tkinter import *
 from tkinter import ttk
+from tokenize import String
 
 def enter_pressed():
     print(entry_value.get())
+    label.configure(text=entry_value.get())
     writing_entry.delete(0, 'end')
 
 root = Tk()
@@ -15,5 +17,7 @@ writing_entry = ttk.Entry(root, textvariable=entry_value, width=20)
 writing_entry.grid(row=0,columnspan=3)
 enter_btn = ttk.Button(root, text='entr', command=enter_pressed)
 enter_btn.grid(row=0, column=4)
+label = ttk.Label(root, text="there's nothing now")
+label.grid(row=1, columnspan=3)
 
 root.mainloop()
